9ece1e922706375be4dd5293486e8952763fc6a1
Cocoa-Bunny <jahvon.k.jones@gmail.com>
2026-04-05 23:01:10 -0400

    Update SpellBook.java

--- src/com/example/myapplication/SpellBook.java
+++ src/com/example/myapplication/SpellBook.java
@@ -1,6 +1,16 @@
 package com.example.myapplication;
 
-public class SpellBook implements PlayerStrategy{
+public class SpellBook implements PlayerStrategy {
+    
+    private String name;
+
+    /*
+        TODO: Make a no-args constructor that will initialize this field to "Spell Book"
+     */
+    public SpellBook() {
+        this.name = "Spell Book";
+    }
+
     /*
         TODO: Implement the player Strategy methods so that they behave in accordance with the assignment details
             These methods should print the following: “{name of pattern} - {size of attack}” where each of the braced
@@ -8,8 +18,23 @@ public class SpellBook implements PlayerStrategy{
             One additional method should announce just the name of the strategy
         Hint: There are four methods that you need to implement from the PlayerStrategy class
      */
+    @Override
+    public void attackSmall() {
+        System.out.println(this.name + " - Small");
+    }
 
-    /*
-        TODO: Make a no-args constructor that will initialize this field to "Spell Book"
-     */
+    @Override
+    public void attackMedium() {
+        System.out.println(this.name + " - Medium");
+    }
+
+    @Override
+    public void attackLarge() {
+        System.out.println(this.name + " - Large");
+    }
+
+    @Override
+    public void announceStrategy() {
+        System.out.println(this.name);
+    }
 }
